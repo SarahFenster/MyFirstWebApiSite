f4e4e8624b35e085bf1bfacf37fcf7947caf3fdd
326023306 <326023306@PC9-10>
2023-11-01 12:00:21 +0200

    Add Validations

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Entities;
 
 public partial class User
 {
     public int Id { get; set; }
-
+    [EmailAddress(ErrorMessage = "The UserName field must have a email address format")]//how?
     public string? UserName { get; set; }
-
+    [StringLength(10, ErrorMessage = "Password length can't be more than 10, choose a different password")]//how?
     public string? Password { get; set; }
 
     public string? FirstName { get; set; }
