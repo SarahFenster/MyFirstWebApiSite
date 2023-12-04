54db16e06b44e7c3ee597acf8640d70242f6f25a
SarahFenster <rf0533102290@gmail.com>
2023-12-04 16:00:49 +0200

    More little fixes, project is completed!

--- MyFirstWebApiSite/wwwroot/UserDetails.js
+++ MyFirstWebApiSite/wwwroot/UserDetails.js
@@ -1,16 +1,13 @@
 
 
 const showUpdateTags = () => {
-    const updateTags = document.getElementById("update")
+    let updateTags = document.getElementById("update")
     updateTags.style.visibility = "initial" 
-    const user = JSON.parse(sessionStorage.getItem("User"))
-     document.getElementById("updateName").value=user.userName
-    const password=document.getElementById("updatePassword").value = user.password
+    let user = JSON.parse(sessionStorage.getItem("User"))
+    document.getElementById("updateName").value=user.userName
+    let password=document.getElementById("updatePassword").value = user.password
     document.getElementById("updateFName").value = user.firstName
     document.getElementById("updateLName").value = user.lastName
-    let result = fetchPwdStrength(password)
-    document.getElementById("progress").value = result / 4;
-   
 }
 
 const fetchPwdStrength = async (password) => {
@@ -34,16 +31,14 @@ const fetchPwdStrength = async (password) => {
 
 async function updateUserDetails() {
     try {
-        const userName = document.getElementById("updateName").value
-        const password = document.getElementById("updatePassword").value
-        const firstName = document.getElementById("updateFName").value
-        const lastName = document.getElementById("updateLName").value
-        const user = { userName, password, firstName, lastName }
-        const result = fetchPwdStrength(password)
+        let userName = document.getElementById("updateName").value
+        let password = document.getElementById("updatePassword").value
+        let firstName = document.getElementById("updateFName").value
+        let lastName = document.getElementById("updateLName").value
+        let user = { userName, password, firstName, lastName }
+        let result = fetchPwdStrength(password)
         if (result < 2) {
             alert("easy password... choose a differrent one")
-            const progress = document.getElementById("progress")
-            progress.value = result / 4
             return
         }
         const storagedUser = JSON.parse(sessionStorage.getItem("User"))
